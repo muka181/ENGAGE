f1411f2f117c65419b89684c419c86f938a8a96e
muka181 <mukamag78@gmail.com>
2024-04-29 16:51:48 +0300

    second submission

---
+++ javascript.js
@@ -0,0 +1,30 @@
+let text = document.querySelector("#text")
+
+text.style.padding = "20px"
+
+text.innerHTML = `  
+                     <ul class="mainlist">
+                      <div class="approach_section">
+                         <div> 
+                            <h1 class="mission_dg approach">Our approach</h1>
+                            <p class="mission_dg font_setup">Foundations</p>
+                            <li class="mission_dg">
+                            Develop a public health focussed machine learning curriculum
+                            </li>
+                         </div>
+                         <div> 
+                             <p class="mission_dg font_setup">Collaborations</p>
+                             <li class="mission_dg">
+                             Collaborate with regional universities and community organizations to reach young women 
+                             in rural and underserved areas
+                             </li>
+                         </div>
+                         <div> 
+                             <p class="mission_dg font_setup">Training</p>
+                            <li class="mission_dg">
+                            Train these women in machine learning through these universities and provide paid internships for career growth
+                            </li>
+                         </div> 
+                      </div>
+                     </ul>
+`
